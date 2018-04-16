0209eb5e4be702cb9aab533788365a6088052c76
shuqi <821286843@qq.com>
2018-04-16 10:58:15 +0800

    init

---
+++ ch5/src/main/java/com/akkademy/ParseArticle.java
@@ -0,0 +1,8 @@
+package com.akkademy;
+
+public class ParseArticle {
+    public final String htmlBody;
+    public ParseArticle(String url) {
+        this.htmlBody = url;
+    }
+}
---
+++ ch8/src/test/java/com/akkachat/PingPongActorTest.java
@@ -0,0 +1,61 @@
+package com.akkachat;
+
+import akka.actor.ActorSystem;
+
+public class PingPongActorTest {
+
+    static ActorSystem system;
+//
+//    @BeforeClass
+//    public static void setup() {
+//        system = ActorSystem.create();
+//    }
+//
+//    @AfterClass
+//    public static void tearDown() {
+//        JavaTestKit.shutdownActorSystem(system);
+//        system = null;
+//    }
+//
+//    @Test
+//    public void testPingActor() {
+//        new JavaTestKit(system) {{
+//            final ActorRef pingActor = system.actorOf(PingActor.props());
+//            pingActor.tell(new PongActor.PongMessage("pong"), getRef());
+//
+//            final String pingResult = new ExpectMsg<String>("ping message") {
+//                // do not put code outside this method, will run afterwards
+//                protected String match(Object in) {
+//                    if (in instanceof PingActor.PingMessage) {
+//                        PingActor.PingMessage ping = (PingActor.PingMessage) in;
+//                        return ping.getText();
+//                    }
+//                    throw noMatch();
+//                }
+//            }.get(); // this extracts the received message
+//
+//            assertEquals("ping", pingResult);
+//        }};
+//    }
+//
+//    @Test
+//    public void testPongActor() {
+//        new JavaTestKit(system) {{
+//            final ActorRef pongActor = system.actorOf(PongActor.props());
+//            pongActor.tell(new PingActor.PingMessage("ping"), getRef());
+//
+//            final String pongResult = new ExpectMsg<String>("pong message") {
+//                // do not put code outside this method, will run afterwards
+//                protected String match(Object in) {
+//                    if (in instanceof PongActor.PongMessage) {
+//                        PongActor.PongMessage pong = (PongActor.PongMessage) in;
+//                        return pong.getText();
+//                    }
+//                    throw noMatch();
+//                }
+//            }.get(); // this extracts the received message
+//
+//            assertEquals("pong", pongResult);
+//        }};
+//    }
+}
